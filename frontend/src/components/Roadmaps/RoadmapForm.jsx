d219bb4a464c27901ad4109dc447b03520317e38
Nikita Khabya <nikitakhabya03@gmail.com>
2022-09-25 16:08:34 +0530

    added ceckout page, customized tailwind classes

--- frontend/src/components/Roadmaps/RoadmapForm.jsx
+++ frontend/src/components/Roadmaps/RoadmapForm.jsx
@@ -17,87 +17,88 @@ export const RoadmapForm = () => {
         <div className='relative'>
             <img
                 className='h-screen w-full'
-                src='https://images.unsplash.com/photo-1505682634904-d7c8d95cdc50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' />
+                src='https://images.unsplash.com/photo-1497091071254-cc9b2ba7c48a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzd8fHJvYWRtYXB8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+                alt='bg' />
             <div className=''>
                 <form
                     className='absolute left-0 top-0 bottom-0 text-gray-300 font-serif'>
-                    <h1 className='text-6xl mt-10 ml-10 font-semibold '>Add a new Roadmap</h1>
+                    <h1 className='text-6xl mt-10 ml-10 font-semibold'>Add a new Roadmap</h1>
 
                     <div className='grid grid-cols-2'>
                         <div className='flex flex-col'>
-                            <label className='  font-bold text-xl p-3 ml-10'>Name of Roadmap</label>
+                            <label className='  form-label'>Name of Roadmap</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input'
                                 placeholder="Name of the roadmap"
                                 type="text"
                                 onChange={(e) => setName(e.target.value)}
                                 value={name}
                             />
-                            <label className=' font-bold text-xl p-3 ml-10'>Description</label>
+                            <label className=' form-label'>Description</label>
                             <textarea
                                 rows={4}
-                                className='ml-10 rounded-md p-1'
+                                className='form-input'
                                 placeholder='Description of roadmap'
                                 type="text"
                                 onChange={(e) => setDesc(e.target.value)}
                                 value={desc}
                             />
-                            <label className=' font-bold text-xl p-3 ml-10'>Skills required</label>
+                            <label className=' form-label'>Skills required</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input'
                                 placeholder='Add skillset required '
                                 type="text"
                                 onChange={(e) => setskills(e.target.value)}
                                 value={skills}
                             />
-                            <label className=' font-bold text-xl p-3 ml-10'>Entrance Exams</label>
+                            <label className=' form-label'>Entrance Exams</label>
                             <input
-                                className='ml-10 rounded-md p-1'
-                                placeholder='Entrance exams '
+                                className='form-input'
+                                placeholder='Entrance exams'
                                 type="text"
                                 onChange={(e) => setExam(e.target.value)}
                                 value={exam}
                             />
                         </div>
                         <div className='flex flex-col'>
-                            <label className='  font-bold text-xl p-3 ml-10'>Streams</label>
+                            <label className='  form-label'>Streams</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input bg-gray-100'
                                 placeholder="Stream required "
                                 type="text"
                                 onChange={(e) => setStream(e.target.value)}
                                 value={stream}
                             />
 
-                            <label className=' font-bold text-xl p-3 ml-10'>Higher Education</label>
+                            <label className=' form-label'>Higher Education</label>
                             <input
                                 rows={4}
-                                className='ml-10 rounded-md p-1'
+                                className='form-input bg-gray-100'
                                 placeholder='Higher Eductaion '
                                 type="text"
                                 onChange={(e) => setHEdu(e.target.value)}
                                 value={HEdu}
                             />
 
-                            <label className=' font-bold text-xl p-3 ml-10'>Top Recruiters</label>
+                            <label className='form-label'>Top Recruiters</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input bg-gray-100'
                                 placeholder='Top recruiters '
                                 type="text"
                                 onChange={(e) => setRecruiters(e.target.value)}
                                 value={Recruiters}
                             />
-                            <label className=' font-bold text-xl p-3 ml-10'>Duration</label>
+                            <label className='form-label'>Duration</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input'
                                 placeholder='Duration'
                                 type="text"
                                 onChange={(e) => setDuration(e.target.value)}
                                 value={duration}
                             />
-                            <label className=' font-bold text-xl p-3 ml-10'>Job Profiles</label>
+                            <label className=' form-label'>Job Profiles</label>
                             <input
-                                className='ml-10 rounded-md p-1'
+                                className='form-input'
                                 placeholder='Job Profile'
                                 type="text"
                                 onChange={(e) => setJob(e.target.value)}
